df33e7d762d8f5636d6efa102ed461f5bc911e82
ginglis13 <ginglis05@gmail.com>
2020-05-20 18:12:02 -0400

    css: looks ok on mobile now

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,23 +10,18 @@ import React from 'react';
 export default Footer;
 */
 var style = {
-  //backgroundColor: "#F8F8F8",
   backgroundColor: "#282c34",
   color: "white",
   borderTop: "1px solid #E7E7E7",
   textAlign: "center",
-  padding: "20px",
   position: "fixed",
   left: "0",
   bottom: "0",
-  height: "60px",
   width: "100%",
 }
 
 var phantom = {
 display: 'block',
-padding: '20px',
-height: '60px',
 width: '100%',
 }
 
